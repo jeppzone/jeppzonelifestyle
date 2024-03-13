0b4af33cdd59c13e9951c886eab22c6847850f7b
Jesper Olsson Laine <jespols@student.chalmers.se>
2024-03-13 15:12:11 +0100

    Revert "Add spinner"
    This reverts commit 0f14384a9aee8c35f3b829bbce266c8644866f42.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,25 @@
 <script>
-	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
-	import Spinner from '$lib/Spinner.svelte';
 	import '../app.css';
 </script>
 
 <div class="app bg-gradient-to-r from-slate-950 to-slate-900">
-	{#if !browser}
-		<div class="mx-auto">
-			<Spinner />
-		</div>
-	{:else}
-		<header>
-			{#if $page.url.pathname !== '/'}
-				<nav class="w-full grid justify-center">
-					<a href="/" class="text-white border border-white p-5 rounded-l mt-10">Home</a>
-				</nav>
-			{/if}
-		</header>
-		<main>
-			<slot />
-		</main>
-		<footer class="text-pretty pt-10 text-center tracking-tighter">
-			<p>
-				Disclaimer: The contents of this site is for informational purposes only and should not be
-				seen as medical advice
-			</p>
-		</footer>
-	{/if}
+	<header>
+		{#if $page.url.pathname !== '/'}
+			<nav class="w-full grid justify-center">
+				<a href="/" class="text-white border border-white p-5 rounded-l mt-10">Home</a>
+			</nav>
+		{/if}
+	</header>
+	<main>
+		<slot />
+	</main>
+	<footer class="text-pretty pt-10 text-center tracking-tighter">
+		<p>
+			Disclaimer: The contents of this site is for informational purposes only and should not be
+			seen as medical advice
+		</p>
+	</footer>
 </div>
 
 <style>
