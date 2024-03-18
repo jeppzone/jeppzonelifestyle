89e70273de9210028ded060b9737468c929edd96
Jesper Olsson Laine <jespols@student.chalmers.se>
2024-03-18 15:02:47 +0100

    Fix sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -5,22 +5,31 @@ export async function GET({ fetch, setHeaders }) {
 		'Content-Type': 'application/xml'
 	});
 
-	const site = 'https://www.jeppzonelifestyle.com';
+	const sites = [
+		'https://www.jeppzonelifestyle.com',
+		'https://www.jeppzonelifestyle.com/tools/child-height-calculator',
+		'https://www.jeppzonelifestyle.com/posts'
+	];
+	const baseUrl = 'https://www.jeppzonelifestyle.com';
 	const response = await fetch('api/posts');
 	const posts = await response.json();
 
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
                 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                <url>
+                ${sites
+									.map(
+										(site) => `<url>
                 <loc>${site}</loc>
                 <changefreq>daily</changefreq>
                 <priority>0.7</priority>
-                </url>
+                </url>`
+									)
+									.join('')}
                 ${posts
 									.map(
 										(post) => `
                 <url>
-                <loc>${site}/posts/${post.slug}</loc>
+                <loc>${baseUrl}/posts/${post.slug}</loc>
                 <changefreq>weekly</changefreq>
                 <lastmod>${post.date}</lastmod>
                 </url>
